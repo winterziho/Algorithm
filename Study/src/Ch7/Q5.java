379a035434bfd16bc842a581d5bd466ee879193b
lim <winterziho@gmail.com>
2024-07-30 17:27:31 +0900

    dfs study

---
+++ Study/src/Ch7/Q5.java
@@ -0,0 +1,39 @@
+package Ch7;
+
+class Node{
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		data = val;
+		lt = rt = null;
+	}
+}
+
+public class Q5 {
+
+	Node root;
+	public void DFS(Node root) {
+		if(root == null) return;
+		else {
+			
+		
+			DFS(root.lt);
+			System.out.print(root.data);
+			DFS(root.rt);
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Q5 tree = new Q5();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		tree.root.rt.lt = new Node(6);
+		tree.root.rt.rt = new Node(7);
+		tree.DFS(tree.root);
+	}
+}
